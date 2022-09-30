111d74dfb8549fa2da9e3035aac2e37ea6366606
floratoth <a.flora95@gmail.com>
2022-09-30 15:14:58 +0200

    Create accordion, accordion-item components, add db

--- accordion/src/app/app.module.ts
+++ accordion/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AccordionItemComponent } from './accordion-item/accordion-item.component';
+import { AccordionComponent } from './accordion/accordion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccordionItemComponent,
+    AccordionComponent
   ],
   imports: [
     BrowserModule
