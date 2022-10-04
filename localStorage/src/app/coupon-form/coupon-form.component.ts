63f603f047bbc09afebf6b5733787791698eb33b
floratoth <a.flora95@gmail.com>
2022-10-04 13:58:45 +0200

    Add localStorage set and get logic

--- localStorage/src/app/coupon-form/coupon-form.component.ts
+++ localStorage/src/app/coupon-form/coupon-form.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-coupon-form',
@@ -18,5 +23,56 @@ export class CouponFormComponent implements OnInit {
     });
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.fullName.setValue(this.setInputValue('fullName'));
+    this.email.setValue(this.setInputValue('email'));
+    this.phoneNumber.setValue(this.setInputValue('phoneNumber'));
+    this.couponCode.setValue(this.setInputValue('couponCode'));
+  }
+
+  get fullName(): FormControl {
+    return this.couponForm.get('fullName') as FormControl;
+  }
+
+  get email(): FormControl {
+    return this.couponForm.get('email') as FormControl;
+  }
+
+  get phoneNumber(): FormControl {
+    return this.couponForm.get('phoneNumber') as FormControl;
+  }
+
+  get couponCode(): FormControl {
+    return this.couponForm.get('couponCode') as FormControl;
+  }
+
+  setLocalStorage(from: string, to: string): void {
+    localStorage.setItem(from, to);
+  }
+
+  getItemFromLocalStorage(key: string): string | null {
+    return localStorage.getItem(key);
+  }
+
+  setInputValue(key: string): any {
+    let value = this.getItemFromLocalStorage(key);
+    if (value === null) {
+      return '';
+    } else {
+      return JSON.parse(value);
+    }
+  }
+
+  onSubmit(): void {
+    console.log(this.couponForm.value);
+    let formDataList = [
+      { key: 'fullName', value: this.fullName.value },
+      { key: 'email', value: this.email.value },
+      { key: 'phoneNumber', value: this.phoneNumber.value },
+      { key: 'couponCode', value: this.couponCode.value },
+    ];
+    formDataList.map((item) => {
+      this.setLocalStorage(item.key, JSON.stringify(item.value));
+    });
+  }
 }
